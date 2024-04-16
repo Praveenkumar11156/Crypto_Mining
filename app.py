8f3cd206ce373fb34301e7b73a519703d95975a5
PRAVEEN KUMAR <91111675+Praveenkumar11156@users.noreply.github.com>
2024-04-16 17:22:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
-from flask import Flask
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask import render_template
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-from matplotlib.cm import rainbow
-
 import warnings
 warnings.filterwarnings('ignore')
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
-
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import VotingClassifier
-from sklearn import linear_model
 
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
-#################################################
-# Flask Routes
-#################################################
 
 @app.route("/")
 def index():
